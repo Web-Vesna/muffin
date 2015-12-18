56dafb9ed383e228c26ce5b76c49490b50da1ef1
bakit <bakit@debian>
2015-12-18 14:05:07 +0300

    asyncio & aiohttp updated

--- capuccino/config_parser.py
+++
@@ -1,4 +0,0 @@
-class ServiceConfig():
-	def __init__(self, service):
-		for key, value in service.items():
-			setattr(self, key, value)
--- capuccino/urls.py
+++
@@ -1,8 +0,0 @@
-from handlers.handlers import TCPHandler
-from handlers.handlers import BaseHandler
-
-urls = [
-	(r'/', BaseHandler),
-	(r'/test', TCPHandler),
-
-]
